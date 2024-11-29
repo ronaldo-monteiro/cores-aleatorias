7ef25a1ce1882ae9f16b3cf83d5f5181370b8540
main <ronaldomont@hotmail.com>
2024-11-29 03:06:19 -0300

    Terminando o codigo e indo dormir

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ function gerarCorAleatoria() {
     const r = Math.floor(Math.random() * 256); // Gera um valor aleatório entre 0 e 255 para o vermelho
     const g = Math.floor(Math.random() * 256); // Gera um valor aleatório entre 0 e 255 para o verde
     const b = Math.floor(Math.random() * 256); // Gera um valor aleatório entre 0 e 255 para o azul
-    return `rgb(${r}, ${g}, ${b})`; // Retorna a cor no formato RGB
+    return `RGB(${r}, ${g}, ${b})`; // Retorna a cor no formato RGB
 }
 
 // Adiciona um ouvinte de evento para o clique no quadrado
